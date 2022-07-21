d587328568aef3d33cdba6358a6127c423750bc1
Bruno Eduardo <brunotecsi@gmail.com>
2022-07-21 08:42:51 -0300

    pagina principal e about-us criada

---
+++ script.js
@@ -0,0 +1,25 @@
+let time = 2000,
+   currentImageIndex = 0,
+   images = document.querySelectorAll("#carrousel img")
+   max = 3;
+
+
+    function nextImage(){
+
+        images[currentImageIndex].classList.remove("selected")        
+        currentImageIndex++
+        
+        if (currentImageIndex >=max)
+                currentImageIndex = 0;
+
+        images[currentImageIndex].classList.add("selected")
+    }
+
+
+   function start(){
+        setInterval(()=>{
+            nextImage();
+        },time)
+   }
+
+   window.addEventListener("load",start)
