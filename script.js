754c53695d5f10ec3cebc5a865e0e81f3da741e5
Bruno Eduardo <brunotecsi@gmail.com>
2022-07-24 18:26:22 -0300

    finalizando pagina inicial e adicionando funções aos botões

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+const carrosel1 = document.getElementById("carroussel1")
+const carrosel2 = document.getElementById("carroussel2")
+const carrosel3 = document.getElementById("carroussel3")
+
+
 let time = 3000,
    currentImageIndex = 0,
    images = document.querySelectorAll("#carrousel img")
@@ -24,3 +30,28 @@ let time = 3000,
    }
 
    window.addEventListener("load",start)
+
+   function buttonNext() {
+
+    if (carrosel1.style.display="block") {
+        
+        carrosel2.style.display="block"
+        carrosel1.style.display="none"
+    }
+
+    if (carrosel2.style.display="block") {
+        $('#carroussel3').css('display','block')
+        $('#carroussel2').css('display','none')
+    }
+    if (carrosel3.style.display="block") {
+        $('#next').css('color','gray')
+        $('#arrow-next').css('color','gray')
+
+    }
+
+   }
+
+   function buttonPrev() {
+
+}
+
